413e6191459a13812f605da02453af804558fdb3
diana <zS22017039@estudiantes.uv.mx>
2024-05-26 00:08:00 -0600

    I added the AddressBook and AddressBookTest classes

--- ProyectoPrincipios/demo/src/main/java/com/example/AddressBook.java
+++ ProyectoPrincipios/demo/src/main/java/com/example/AddressBook.java
@@ -1,5 +1,47 @@
 package com.example;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class AddressBook {
+    
+    private List<AddressEntry> listOfRegisters;
+
+    public AddressBook() {
+        this.listOfRegisters = new ArrayList<>();
+    }
+
+    public void addNewRegister(AddressEntry userInput) {
+        listOfRegisters.add(userInput);
+    }
+
+    public void removeRegister(AddressEntry userInput) {
+        listOfRegisters.remove(userInput);
+    }
+
+    public List<AddressEntry> searchRegister(String lastName) {
+        List<AddressEntry> searchResults = new ArrayList<>();
+        for (int i = 0; i < listOfRegisters.size(); i++) {
+            AddressEntry userInput = listOfRegisters.get(i);
+            if (userInput.getLastName().equals(lastName)) {
+                searchResults.add(userInput);
+            }
+        }
+        return searchResults;
+    }
+
+    public List<AddressEntry> getAllRegisters() {
+        return listOfRegisters;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (int i = 0; i < listOfRegisters.size(); i++) {
+            AddressEntry userInput = listOfRegisters.get(i);
+            result = result + userInput.toString() + "\n";
+        }
+        return result;
+    }
     
 }
