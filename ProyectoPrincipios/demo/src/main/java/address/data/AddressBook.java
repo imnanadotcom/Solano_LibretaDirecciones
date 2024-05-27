be6a9fc5b99f604434c93e341e2a96ed2e8000a8
diana <zS22017039@estudiantes.uv.mx>
2024-05-26 23:13:16 -0600

    UML Class Diagram update

--- ProyectoPrincipios/demo/src/main/java/address/data/AddressBook.java
+++ ProyectoPrincipios/demo/src/main/java/address/data/AddressBook.java
@@ -72,7 +72,7 @@ public class AddressBook {
     }
 
     public void openTXT(String filename) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'openTXT'");
     }
     
